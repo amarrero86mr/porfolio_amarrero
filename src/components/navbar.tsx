5350266ebd8aaf0a08e8e7bc05b64db1cc7f08f2
amarrero86mr <amarrero86mr@gmail.com>
2025-06-18 18:10:53 -0300

    creando navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,22 @@
+
+
+
+export const Navbar = () => {
+
+    return(
+        <nav className="nav" id="navbar">
+        <div className="nav-content">
+          <h3>_agustin_m_marrero</h3>
+          <ul className="nav-items">
+            <li className="nav-item">HOME</li>
+            <li className="nav-item">ABOUT_ME</li>
+            <li className="nav-item">SKILLs</li>
+            <li className="nav-item">PROJECTs</li>
+            <li className="nav-item">CERTIFICATEs</li>
+            <li className="nav-item">Contact</li>
+          </ul>
+        </div>
+      </nav>
+
+    )
+}
