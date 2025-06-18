2c91f833f108ff5a97b9d56a10ee66aa60cd90b7
amarrero86mr <amarrero86mr@gmail.com>
2025-06-18 18:07:15 -0300

    Creando estuctura base del proyeto - carpetas y archivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { Navbar } from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="font-sans text-gray-800 scroll-smooth">
+      <Navbar />
+      <main>
+        <section id="hero"><Hero /></section>
+        <section id="about"><About /></section>
+        <section id="certificates"><Skills /></section>
+        <section id="projects"><Projects /></section>
+        <section id="certificates"><Certificates /></section>
+        <section id="contact"><Contact /></section>
+      </main>
+      <Footer />
+    </div>
       <h1>Proyec Portfolio</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
