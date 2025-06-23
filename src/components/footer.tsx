0d42e4438f7ae62a5d49c29e2f182cba5fb64cc8
amarrero86mr <amarrero86mr@gmail.com>
2025-06-23 19:55:27 -0300

    agregado de icono de discord

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -12,6 +12,9 @@ export const Footer = () => {
                     <li className="nav-item"><a target="_blank" href="https://github.com/amarrero86mr">
                         <img src="src/assets/icons/github_icon.svg" alt="icono de github" width={'40px'} />
                     </a></li>
+                    <li className="nav-item"><a target="_blank" href="https://discord.com/channels/@amarrero86mr#1592">
+                        <img src="src/assets/icons/discord_icon.svg" alt="icono de discord" width={'40px'} />
+                    </a></li>
                 </ul>
             </div>
         </nav>
