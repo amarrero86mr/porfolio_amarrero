f859eeddbdbfabac4ebee5516ad03e338b8295a4
amarrero86mr <amarrero86mr@gmail.com>
2025-08-23 13:11:30 -0300

    agregando conteo de skills seleccionadas en navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import { DarkLightContext, type TDarkLightContext } from "./darklight.context"
 import { Link } from 'react-scroll';
 import imag_sun from "../assets/icons/sun.svg"
 import imag_moon from "../assets/icons/moon.svg"
+import { SkillContext, type TSkillContext } from "./skills.contexty";
 export const Navbar = () => {
 
   const { changeTheme, fnChangeTheme } = useContext<TDarkLightContext>(DarkLightContext)
@@ -12,6 +13,7 @@ export const Navbar = () => {
     fnChangeTheme();
   }
 
+  const { skillSelected } = useContext<TSkillContext>(SkillContext)
   return (
     <div>
       <div className="z-10 fixed top-0 p-3 h-16 w-full backdrop-blur-md " ></div>
@@ -28,7 +30,7 @@ export const Navbar = () => {
                 onClick={() => {
                   fnThem()
                 }}>{changeTheme == 'lightTheme'
-                  ? <img className="" src={imag_moon} alt="icono de luna" width={'30px'}/>
+                  ? <img className="" src={imag_moon} alt="icono de luna" width={'30px'} />
                   : <img src={imag_sun} alt="icono de sol" width={'30px'} />}</button></li>
             <li className={`nav-item p-1 ` + changeTheme}>
               <Link to="hero" spy={true} smooth={true} duration={500}>HOME</Link> </li>
@@ -41,7 +43,10 @@ export const Navbar = () => {
             <li className={`nav-item p-1 ` + changeTheme}>
               <Link to="certificates" spy={true} smooth={true} duration={500}>CERTIFICATEs</Link> </li>
             <li className={`nav-item p-1 ` + changeTheme}>
-              <Link to="contact" spy={true} smooth={true} duration={500}>CONTACT</Link> </li>
+              <Link to="contact" spy={true} smooth={true} duration={500}>CONTACT {skillSelected.length > 0
+                ? <span className="bg-rose-500 z-30 text-rose-50 rounded-full text-xs h-4 w-4 m-1 absolute right-8 top-7 text-center "
+              >{skillSelected.length}<span className="bg-rose-500 z-20 rounded-full h-4 w-4 m-1 fixed top-7 right-8 animate-ping">{skillSelected.length}</span></span>
+              : null} </Link> </li>
           </ul>
         </div>
       </nav>
